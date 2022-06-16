e9c2c3b11477143ab9d28443dbb9f2fb0dc5746b
S-Spiegl <steven.spiegl@gmail.com>
2022-06-16 14:21:43 +0100

    can add session id to friends array of desired friend

--- routes/users.js
+++ routes/users.js
@@ -11,10 +11,17 @@ router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 //5. when a request is made to this route (i.e. when someone signs up) 
 //the user controller runs create
-router.post("/friend-added", UsersController.AddFriend);
+router.post("/add-friend", UsersController.AddFriend);
 //confused... not sure I'm using this properly
 //my understanding, if we post to users/add-friend, then we run 
 // AddFriend in the controller to create a friend using data supplied 
 //in the form from which we're making the post request
 
 module.exports = router;
+
+//make a new route for friends so we can use a session-checker to see if
+//someone is logged in
+//change form to route to new routes file and thus new handler
+//we can now update the target friend's friends array, we need to be able to update
+//the user's friends array
+//
