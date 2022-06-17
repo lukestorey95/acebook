d0c345cd95a6b0fb4c824ec0bc9e99edf985310b
Erlantz <erlantzram@gmail.com>
2022-06-17 14:11:23 +0100

    solution implemented. Removed req.cookies from sessionChecker

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 
 // middleware function to check for logged-in users
 const sessionChecker = (req, res, next) => {
-  if (!req.session.user && !req.cookies.user_sid) {
+  if (!req.session.user) {
     res.redirect("/sessions/new");
   } else {
     next();
